892e2575d4e8440ec93488a5a6b1add941616da8
cullen6732 <cullen6732@gmail.com>
2023-12-15 21:57:13 +0900

    JS-Home-opacity

---
+++ src/main.js
@@ -0,0 +1,19 @@
+// Header에 페이지 아래로 스크롤시 다크 스타일링 적용
+const header = document.querySelector('.header');
+const headerHeight = header.getBoundingClientRect().height;
+document.addEventListener('scroll', () => {
+  // Header에 페이지 아래로 스크롤시 다크 스타일링 적용
+  if (window.scrollY > headerHeight) {
+    header.classList.add('header--dark');
+  } else {
+    header.classList.remove('header--dark');
+  }
+});
+
+const home = document.querySelector('.home__container');
+const homeHeight = home.offsetHeight;
+document.addEventListener('scroll', () => {
+  // Home 섹션을 아래로 스크롤시 투명하게 처리함
+  console.log(1 - window.scrollY / homeHeight);
+  home.style.opacity = 1 - window.scrollY / homeHeight;
+});
